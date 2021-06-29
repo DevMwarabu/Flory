9a0a5c7e575a72ba884c3429806eb526ec4efa09
DevMwarabu <johnmwarabuchone@gmail.com>
2021-06-29 14:50:24 +0300

    Frags

--- app/src/main/java/com/laxco/gardening/Dashboard.java
+++ app/src/main/java/com/laxco/gardening/Dashboard.java
@@ -1,14 +1,60 @@
 package com.laxco.gardening;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
+import com.google.android.material.appbar.AppBarLayout;
+import com.google.android.material.tabs.TabLayout;
+import com.laxco.gardening.PageAdapter.PagerAdapter;
+
 public class Dashboard extends AppCompatActivity {
+    private AppBarLayout mAppBarLayout;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private PagerAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        tabLayout = findViewById(R.id.tablayout_main);
+        viewPager = findViewById(R.id.viewPager);
+
+        //adding tabs
+        tabLayout.addTab(tabLayout.newTab());
+        tabLayout.addTab(tabLayout.newTab());
+        tabLayout.addTab(tabLayout.newTab());
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        //setting viewPager adapter
+        adapter = new PagerAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
+
+        //setting tab titles
+        tabLayout.getTabAt(0).setText("Indoors");
+        tabLayout.getTabAt(1).setText("Outdoors");
+        tabLayout.getTabAt(2).setText("Gardening");
+
+        viewPager.addOnPageChangeListener((ViewPager.OnPageChangeListener) new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @SuppressLint("NewApi")
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/laxco/gardening/Splashscreen.java
+++ app/src/main/java/com/laxco/gardening/Splashscreen.java
@@ -2,13 +2,54 @@ package com.laxco.gardening;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.airbnb.lottie.LottieAnimationView;
+import com.laxco.gardening.Introduction.PrefManager;
+import com.laxco.gardening.Introduction.WelcomeActivity;
+
 public class Splashscreen extends AppCompatActivity {
+    private LottieAnimationView mLottieAnimationView;
+    private PrefManager prefManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashscreen);
+        //init
+        prefManager = new PrefManager(this);
+        mLottieAnimationView = findViewById(R.id.lotty_main);
+        loadingApp();
+    }
+
+    private void loadingApp() {
+
+        mLottieAnimationView.addAnimatorListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                //checking if first time
+                if (!prefManager.isFirstTimeLaunch()){
+                    //opening dashboard
+                    startActivity(new Intent(Splashscreen.this, Dashboard.class));
+                    //finishing activity
+                    Splashscreen.this.finish();
+                }else {
+                    //opening dashboard
+                    startActivity(new Intent(Splashscreen.this, WelcomeActivity.class));
+                    //finishing activity
+                    Splashscreen.this.finish();
+                }
+                super.onAnimationEnd(animation);
+            }
+        });
+    }
+
+    //disabling onBackpressed
+    @Override
+    public void onBackPressed() {
+
     }
 }
